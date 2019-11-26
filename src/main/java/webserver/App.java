286f06dd59d537cb625ec689f9f01580a6bd2353
junehoe <br1ansun9@gmail.com>
2019-11-26 09:39:12 -0800

    Renders hello world message on run

--- src/main/java/webserver/App.java
+++ src/main/java/webserver/App.java
@@ -1,7 +1,9 @@
 package webserver;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 
 public class App {
@@ -13,12 +15,20 @@ public class App {
         System.out.println(new App().getGreeting());
 
         try {
-            final ServerSocket server = createServerSocket(5000);
+            final ServerSocket serverSocket = createServerSocket(5000);
             System.out.println("Listening for connection on port 5000...");
+            final Socket clientSocket = serverSocket.accept();
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            out.println("HTTP/1.1 200 OK");
+            out.println("Content-Type: text/html");
+            out.println("\r\n");
+            out.println("<p>Hello World!</p>");
+
             while (true) {
 
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
