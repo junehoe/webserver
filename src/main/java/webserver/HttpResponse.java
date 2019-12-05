e5c2f6c37f37d9f8c7749b919741ad35e70484a4
junehoe <br1ansun9@gmail.com>
2019-12-05 09:18:02 -0800

    Uses HttpResponseBuilder class to format responses

--- src/main/java/webserver/HttpResponse.java
+++ src/main/java/webserver/HttpResponse.java
@@ -1,12 +1,11 @@
 package webserver;
 
 public class HttpResponse {
-    private static final String CRLF = "\r\n";
-
     public static String response(int statusCode, String contentType, String content) {
-        return String.format(
-                "HTTP/1.1 %s %s%sContent-Type: %s; charset=utf-8%s%s%s",
-                statusCode, ResponseCodes.RESPONSE_CODES.get(statusCode), CRLF, contentType, CRLF, CRLF, content
-        );
+        return new HttpResponseBuilder()
+                .withStatusCode(statusCode)
+                .withContentType(contentType)
+                .withContent(content)
+                .build();
     }
 }
