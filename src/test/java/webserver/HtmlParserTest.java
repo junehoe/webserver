162f5548f31843cb4f67190363fbb62b9c5166e4
junehoe <br1ansun9@gmail.com>
2019-12-06 09:09:51 -0800

    Asserts for specific, unique string in html for better testing

--- src/test/java/webserver/HtmlParserTest.java
+++ src/test/java/webserver/HtmlParserTest.java
@@ -6,10 +6,17 @@ import static org.junit.Assert.*;
 
 public class HtmlParserTest {
     @Test
-    public void returnsParsedHtmlStringForHtmlThatExists() throws IOException {
+    public void returnsParsedHtmlStringForIndexHtml() throws IOException {
         String htmlPath = "index.html";
 
-        assertTrue(HtmlParser.parseHtml(htmlPath).contains("<!DOCTYPE html>"));
+        assertTrue(HtmlParser.parseHtml(htmlPath).contains("<h1>Hello World!</h1>"));
+    }
+
+    @Test
+    public void returnsParsedHtmlStringForErrorHtml() throws IOException {
+        String htmlPath = "error.html";
+
+        assertTrue(HtmlParser.parseHtml(htmlPath).contains("<h1>404 Page Not Found</h1>"));
     }
 
     @Test
