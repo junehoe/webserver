44862e3732c35cc37f69eb547efe1c41a7b07d30
junehoe <br1ansun9@gmail.com>
2019-12-16 09:34:22 -0800

    Adds Page class used to hold page constants

---
+++ src/main/java/webserver/Page.java
@@ -0,0 +1,8 @@
+package webserver;
+
+public class Page {
+    static final String TITLE = "\\$title";
+    static final String BODY = "\\$body";
+    static final String TEMPLATE_HTML = "public/template.html";
+    static final String ERROR_HTML = "public/error.html";
+}
