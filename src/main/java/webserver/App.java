047e2ac6d930a3c1580a6809197c24e3e8c5a71f
junehoe <br1ansun9@gmail.com>
2019-12-17 14:03:51 -0800

    Refactors out methods to separate classes

--- src/main/java/webserver/App.java
+++ src/main/java/webserver/App.java
@@ -1,88 +1,23 @@
 package webserver;
 
-import webserver.socket.SocketCreator;
+import webserver.parser.CliParser;
 
-import static webserver.ServerPages.*;
-import static webserver.TodoItems.*;
-
-import java.io.File;
 import java.io.IOException;
-import java.lang.Thread;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.HashMap;
 
 public class App {
-    private static final String BLANK = "";
+    private static final String PORT = "port";
+    private static final String DIR = "dir";
 
     public static void main(String[] args) {
-        String portArg = System.getProperty("p");
-        String dirArg = System.getProperty("dir");
+        int port = CliParser.getPort(System.getProperty(PORT));
+        String directory = CliParser.getDirectory(System.getProperty(DIR));
 
         try {
-            int port = getPort(portArg);
-            final ServerSocket serverSocket = SocketCreator.createServerSocket(port);
-            Router router = new Router();
-            createServerRoutes(router);
-            if (dirArg.equals(BLANK)) {
-                createTodoListRoutes(router);
-            } else {
-                createTodoListRoutes(router, dirArg);
-            }
-            System.out.println("Listening for connection on port " + port + "...");
-            while (true) {
-                Socket clientSocket = SocketCreator.createClientSocket(serverSocket);
-                Thread thread = new Thread(new HttpHandler(clientSocket, router));
-                thread.start();
-            }
+            Server server = new Server(port, directory);
+            server.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int getPort(String port) {
-        if (port != null && InputValidator.isValidPort(port)) {
-            return Integer.parseInt(port);
-        } else if (System.getenv("PORT") != null) {
-            return Integer.parseInt(System.getenv("PORT"));
-        } else {
-            return 5000;
-        }
-    }
-
-    private static void createServerRoutes(Router router) throws IOException {
-        createRoute(router, INDEX_PATH, INDEX_TITLE, INDEX_BODY);
-        createRoute(router, HEALTH_PATH, HEALTH_TITLE, HEALTH_BODY);
-    }
-
-    private static void createTodoListRoutes(Router router) throws IOException {
-        createRoute(router, TODO_PATH, TODO_TITLE, TodoListBuilder.buildList());
-        createRoute(router, TODO_1_PATH, TODO_1_TITLE, TodoListBuilder.buildItem(TODO_1_TITLE));
-        createRoute(router, TODO_2_PATH, TODO_2_TITLE, TodoListBuilder.buildItem(TODO_2_TITLE));
-        createRoute(router, TODO_3_PATH, TODO_3_TITLE, TodoListBuilder.buildItem(TODO_3_TITLE));
-        createRoute(router, TODO_4_PATH, TODO_4_TITLE, TodoListBuilder.buildItem(TODO_4_TITLE));
-        createRoute(router, TODO_5_PATH, TODO_5_TITLE, TodoListBuilder.buildItem(TODO_5_TITLE));
-    }
-
-    private static void createTodoListRoutes(Router router, String directory) throws IOException {
-        File folder = new File(directory);
-        String[] customFiles = folder.list();
-        String todoList = TodoListBuilder.buildList(customFiles);
-        createCustomRoutes(router, directory, customFiles);
-        createRoute(router, TODO_PATH, TODO_TITLE, todoList);
-    }
-
-    private static void createCustomRoutes(Router router, String directory, String[] customFiles) throws IOException {
-        for (int i = 0; i < customFiles.length; i++) {
-            String path = "/todo/" + (i + 1);
-            String fullPath = directory + "/" + customFiles[i];
-            router.addRoute(path, HtmlBuilder.createHtmlString(fullPath));
-        }
-    }
-
-    private static void createRoute(Router router, String path, String title, String body) throws IOException {
-        HashMap<String, String> descriptors = HtmlBuilder.createPageHashMap(title, body);
-        String htmlString = HtmlBuilder.createHtmlString(descriptors);
-        router.addRoute(path, htmlString);
-    }
 }
