62699cf8e50861deba3177e938ae85718b6d702b
junehoe <br1ansun9@gmail.com>
2019-12-19 11:36:50 -0800

    Adds port boundaries to input validator

--- src/main/java/webserver/InputValidator.java
+++ src/main/java/webserver/InputValidator.java
@@ -1,12 +1,23 @@
 package webserver;
 
 public class InputValidator {
+    private static final int LOWER_PORT_BOUND = 1024;
+    private static final int UPPER_PORT_BOUND = 65535;
+
     public static boolean isValidPort(String portString) {
+        int port;
         try {
-            Integer.parseInt(portString);
+            port = Integer.parseInt(portString);
         } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
-        return true;
+        if (isValidPortNumber(port)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isValidPortNumber(int port) {
+        return (port >= LOWER_PORT_BOUND && port <= UPPER_PORT_BOUND);
     }
 }
