186196920251e94e57fc7e6dc714be5d915ff545
junehoe <br1ansun9@gmail.com>
2020-01-07 09:34:11 -0800

    Wraps create page method in try-catch block

--- src/main/java/webserver/todo/TodoPageCreator.java
+++ src/main/java/webserver/todo/TodoPageCreator.java
@@ -6,12 +6,16 @@ import java.util.HashMap;
 import webserver.HtmlBuilder;
 
 public class TodoPageCreator {
-    public static void createTodoPage(String path, String title) throws IOException {
-        String body = TodoListBuilder.buildItem(title);
-        HashMap<String, String> pageDescriptors = HtmlBuilder.createPageDescriptors(title, body);
-        String htmlString = HtmlBuilder.createHtmlString(pageDescriptors);
-        PrintWriter writer = new PrintWriter( path + "/" + title + ".html", "UTF-8");
-        writer.println(htmlString);
-        writer.close();
+    public static void createTodoPage(String path, String title) {
+        try {
+            String body = TodoListBuilder.buildItem(title);
+            HashMap<String, String> pageDescriptors = HtmlBuilder.createPageDescriptors(title, body);
+            String htmlString = HtmlBuilder.createHtmlString(pageDescriptors);
+            PrintWriter writer = new PrintWriter(path + "/" + title + ".html", "UTF-8");
+            writer.println(htmlString);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
