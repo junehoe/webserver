669cd37628e3f9fb8c3567c9394289fce5526baa
junehoe <br1ansun9@gmail.com>
2020-01-07 11:27:36 -0800

    Removes unnecessary tests

--- src/test/java/webserver/router/RouteInitializerTest.java
+++ src/test/java/webserver/router/RouteInitializerTest.java
@@ -1,7 +1,5 @@
 package webserver.router;
 
-import java.io.File;
-import java.io.IOException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,38 +27,4 @@ public class RouteInitializerTest {
 
         assertEquals(11, router.getRoutes().size());
     }
-
-    //@Test
-    //public void createsTodoListRoute() throws IOException {
-    //    todoList.initializeHardCodedList("./public/test");
-    //    RouteInitializer.createTodoListRoutes(router, todoList.getTodoList());
-
-    //    assertEquals(12, router.getRoutes().size());
-    //}
-
-    //@Test
-    //public void createsCustomTodoItemsRoute() throws IOException {
-    //    String directory = "public/test/fake-files";
-    //    File folder = new File(directory);
-    //    File[] customFiles = folder.listFiles();
-    //    todoList.initializeCustomList(customFiles);
-
-    //    RouteInitializer.createTodoListRoutes(router, todoList.getTodoList());
-
-    //    assertEquals(8, router.getRoutes().size());
-    //}
-
-    //@Test
-    //public void throwsNullPointerExceptionIfDirectoryIsInvalid() {
-    //    try {
-    //        String directory = "obviously/fake/path";
-    //        File folder = new File(directory);
-    //        File[] customFiles = folder.listFiles();
-    //        todoList.initializeCustomList(customFiles);
-
-    //        RouteInitializer.createTodoListRoutes(router, todoList.getTodoList());
-    //    } catch (IOException | NullPointerException e) {
-    //        System.out.println("NullPointerException was thrown");
-    //    }
-    //}
 }
