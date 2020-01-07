86073d3985d7bb89db28dfda0c1cc568510d638b
junehoe <br1ansun9@gmail.com>
2020-01-07 11:21:17 -0800

    Extracts html string builder out of createResponse method

--- src/main/java/webserver/controller/GetController.java
+++ src/main/java/webserver/controller/GetController.java
@@ -1,6 +1,7 @@
 package webserver.controller;
 
 import webserver.HtmlBuilder;
+import webserver.parser.HtmlParser;
 import webserver.request.HttpRequest;
 import webserver.response.HttpResponse;
 import webserver.response.HttpStatusCode;
@@ -8,6 +9,7 @@ import webserver.todo.TodoItem;
 import webserver.todo.TodoList;
 import webserver.todo.TodoListBuilder;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.function.Function;
@@ -25,26 +27,26 @@ public class GetController extends Controller {
     }
 
     public Function<HttpRequest, HttpResponse> index = (request) ->
-            createResponse(INDEX_TITLE, INDEX_BODY, OK);
+            createResponse(htmlString(INDEX_TITLE, INDEX_BODY), OK);
 
     public Function<HttpRequest, HttpResponse> healthCheck = (request) ->
-            createResponse(HEALTH_TITLE, HEALTH_BODY, OK);
+            createResponse(htmlString(HEALTH_TITLE, HEALTH_BODY), OK);
 
     public Function<HttpRequest, HttpResponse> showTodoList = (request) ->
-            createResponse(TODO_TITLE, TodoListBuilder.buildList(todoList.getTodoList()), OK);
+            createResponse(htmlString(TODO_TITLE, TodoListBuilder.buildList(todoList.getTodoList())), OK);
 
     public Function<HttpRequest, HttpResponse> createTodoItem = (request) ->
-            createResponse(CREATE_TODO_ITEM_TITLE, CREATE_TODO_ITEM_BODY, OK);
+            createResponse(htmlString(CREATE_TODO_ITEM_TITLE, CREATE_TODO_ITEM_BODY), OK);
 
     public Function<HttpRequest, HttpResponse> showTodoItem = (request) -> {
         for (TodoItem item : todoList.getTodoList()) {
             if (item.getPath().equals(request.getPath())) {
-                String title = item.getTitle();
-                String body = HtmlBuilder.createTodoDetailHtml(title);
-                return createResponse(title, body, OK);
+                File file = item.getFile();
+                String content = HtmlParser.parseHtml(file.getAbsolutePath(), false);
+                return createResponse(content, OK);
             }
         }
-        return createResponse(ERROR_TITLE, ERROR_BODY, NOT_FOUND);
+        return createResponse(htmlString(ERROR_TITLE, ERROR_BODY), NOT_FOUND);
     };
 
     private String htmlString(String title, String body) {
@@ -57,8 +59,7 @@ public class GetController extends Controller {
         }
     }
 
-    private HttpResponse createResponse(String title, String body, HttpStatusCode httpStatusCode) {
-        String content = htmlString(title, body);
+    private HttpResponse createResponse(String content, HttpStatusCode httpStatusCode) {
         return new HttpResponse.Builder("GET")
                 .withStatusCode(httpStatusCode)
                 .withContentLength(content.length())
