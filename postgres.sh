5109d97414cdb48ad6a40eb1ff7f80b250f36644
junehoe <br1ansun9@gmail.com>
2020-01-09 18:43:46 -0800

    Updates travis.yml with postgres script

--- postgres.sh
+++ postgres.sh
@@ -11,6 +11,16 @@ function create_database {
   createdb -O ${USERNAME} ${DATABASE}
 }
 
+function create_table {
+  psql -U $USERNAME -d $DATABASE -c "CREATE TABLE todo (ID SERIAL PRIMARY KEY NOT NULL, TITLE TEXT NOT NULL, COMPLETE BOOLEAN DEFAULT FALSE)"
+}
+
+function create_hardcoded_todos {
+  psql -U $USERNAME -d $DATABASE -c "INSERT INTO todo (TITLE) VALUES ('Walk the dog')"
+  psql -U $USERNAME -d $DATABASE -c "INSERT INTO todo (TITLE) VALUES ('Buy groceries from Whole Foods')"
+  psql -U $USERNAME -d $DATABASE -c "INSERT INTO todo (TITLE) VALUES ('Implement PUT and DELETE')"
+}
+
 if [[ $(psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='"${USERNAME}"'") != '1' ]]
 then
   create_user
@@ -19,5 +29,6 @@ fi
 if [[ $(psql postgres -tAc "SELECT 1 FROM pg_database WHERE datname='"${DATABASE}"'") != '1' ]]
 then
   create_database
-  $(psql -U postgres -d ${DATABASE} -tAc "CREATE TABLE TODO(ID SERIAL PRIMARY KEY NOT NULL, TITLE TEXT NOT NULL, COMPLETE BOOLEAN);")
+  create_table
+  create_hardcoded_todos
 fi
