bd77b2d3c7d73aa24349e878591afa8eade113e5
junehoe <br1ansun9@gmail.com>
2020-01-15 08:05:01 -0800

    Adds Mustache template WIP

---
+++ src/main/java/webserver/pages/Template.java
@@ -0,0 +1,7 @@
+package webserver.pages;
+
+public class Template {
+    public static final String FILTERED_TODO_LIST_TEMPLATE = "src/main/resources/filtered-todo-list-template.mustache";
+    public static final String MAIN_TODO_LIST_TEMPLATE = "src/main/resources/main-listing.mustache";
+    public static final String TODO_ITEM_TEMPLATE = "src/main/resources/todo-item-template.mustache";
+}
